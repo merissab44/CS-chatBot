6b857f42f6c34651a1fde7d88a9e7c1081720758
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2020-09-21 16:03:28 -0700

    Create chatBot.py

---
+++ chatBot.py
@@ -0,0 +1,5 @@
+def get_bot_response(user_response):
+    bot_response_coffee = ["Great morning wake up drink!", "Our most popular is the medium roast", "black is the way to go", "did you know it's helpful to always say the size of the drink first"]
+    bot_response_frapp = ["perfect for outings with friends!", "those can be expensive but totally worth it" , "did you know that baristas don't know the secret menu drinks, always have the ingredients ready"]
+    bot_response_latte = ["I love doing latte art!", "there's a competition every week for baristas", "if you order a drink for here, you automatically get the latte art"]
+    bot_response_refresher = ["my personal favorite!", "those have caffeine as well", "great summer drink"]
